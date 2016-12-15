17da33ef42e6b7095279ffec44d546e6777d9f61
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2016-12-15 13:13:41 +0000

    initial import

---
+++ gap/francy.gd
@@ -0,0 +1,9 @@
+#
+# francy: Interactive Discrete Mathematics in GAP
+#
+# Declarations
+#
+
+#! @Description
+#!   Insert documentation for you function here
+DeclareGlobalFunction( "francy_Example" );
---
+++ init.g
@@ -0,0 +1,7 @@
+#
+# francy: Interactive Discrete Mathematics in GAP
+#
+# Reading the declaration part of the package.
+#
+
+ReadPackage( "francy", "gap/francy.gd");
